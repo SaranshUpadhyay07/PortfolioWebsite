86b866635413f98b707cf654442e7a14762ccaef
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2025-06-29 08:48:55 +0530

    Padding in all screens updated

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -5,9 +5,9 @@ function Hero() {
     <div className="container py-5 my-5 top-container">
       <div className="row align-items-center hero-section">
         {/* Text Section */}
-        <div id="text-main" className="col-md-5 text-main">
-          <h1 className="display-3 mb-4 ">about.</h1>
-          <h3 className="display-6 mb-3">Hey,I am Saransh Upadhyay</h3>
+        <div id="text-main" className="col-lg-5 text-main">
+          <h1 className="display-3">about.</h1>
+          <h3 className="display-6">Hey,I am Saransh Upadhyay</h3>
           <p className="intro fs-5 mb-2">
             A self-motivated tech enthusiast with a passion for startups, machine learning, and finance-tech, especially high-frequency trading.
           </p>
@@ -17,7 +17,7 @@ function Hero() {
         </div>
 
         {/* Image Section */}
-        <div id="img-main" className="col-md-4 text-center">
+        <div id="img-main" className="col-lg-4 text-center">
           <img
             src="https://media.licdn.com/dms/image/v2/D5603AQF5E9HPElXHoQ/profile-displayphoto-scale_400_400/B56ZeoR.U7H8Ag-/0/1750874975264?e=1756339200&v=beta&t=pqHknqahTkWc3wZyZpphs1UfChqfyW9YFxEHspjQ9WE"
             className="img-fluid rounded-circle shadow-sm"
