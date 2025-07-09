5ec6135f21b12bcb7b08c975af7dbe8ca2f93eae
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2025-07-09 22:23:54 +0530

    about-pic and scroll btn edited

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -6,22 +6,23 @@ function Hero() {
       <div className="px-lg-5">
        <div className="hero-section align-items-center row  px-lg-5">
            {/* Text Section */}
-        <div id="text-main" className="col-lg-5 text-main mt-2 ">
+        <div id="text-main" className="col-lg-6 text-main mt-5">
           <h1 className="display-3 mb-1 mt-lg-2">about. <img src="/images/arrow.svg" className="arrow display-3 mb-4"/></h1>
           <h3 className="display-6 fs-2 pb-4">Hey,I am Saransh Upadhyay</h3>
           <p className="intro mb-lg-5" style={{ fontSize: '1.2rem' }}>
             A self-motivated tech enthusiast with a passion for startups, machine learning, and finance-tech, especially high-frequency trading.
           </p>
         </div>
-        <div id="img-project" className="col-lg-5 text-center">
+        <div id="img-project" className="col-lg-4 text-center">
               <img
                 src="/images/about me.png"
-                className="img-fluid d-none d-md-block"
+                className="img-fluid d-none d-lg-block"
                 alt="Portrait of Saransh Upadhyay"
+                id="hero-image"
                 loading="lazy"
               />
         </div>
-        <div className="text-center my-4 d-none d-md-block">
+        <div className="text-center my-4 d-none d-lg-block">
           <button
             className="scroll-down-btn"
             onClick={() => {
