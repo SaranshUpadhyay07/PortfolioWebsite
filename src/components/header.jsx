9281ec96ede0d34e8f46f3121a0a3c70308ae195
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2025-07-09 11:01:16 +0530

    navbar design updated

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
   useEffect(() => {
@@ -20,66 +20,52 @@ function Header() {
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg fixed-top px-4 navbar-div">
+      <nav className="navbar navbar-expand-lg px-4 navbar-div">
+        <NavLink to="/" className="navbar-brand">
+          <img src="/images/SU.png" className="home-icon" alt="Logo" />
+        </NavLink>
 
-          {/* Toggler */}
-          <button
-            className="navbar-toggler text-white bg-light"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-          {/* Collapsible Links */}
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <div className="navbar-nav ms-auto">
-              <Link to="/" className="nav-link me-5 fs-5">Home</Link>
-              <Link to="/about" className="nav-link me-5 fs-5">About</Link>
-              <Link to="/projects" className="nav-link me-5 fs-5">Projects</Link>
-              <Link to="/blog" className="nav-link me-5 fs-5">Blog</Link>
-            </div>
-          </div>
-      </nav>
-
-      {/* Floating Contact Me Button */}
-      <button
-        className="floating-contact"
-        data-bs-toggle="modal"
-        data-bs-target="#staticBackdrop"
-      >
-        Contact Me
-      </button>
-
-      {/* Contact Modal */}
-      <div
-        className="modal fade"
-        id="staticBackdrop"
-        data-bs-backdrop="static"
-        data-bs-keyboard="false"
-        tabIndex="-1"
-      >
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content bg-dark text-white">
-            <div className="modal-header">
-              <h5 className="modal-title">Contact Me</h5>
-              <button type="button" className="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div className="modal-body">
-              <ul className="list-unstyled">
-                <Link to="/" className="nav-link">Home</Link>
-                <Link to="/about" className="nav-link">About</Link>
-                <Link to="/projects" className="nav-link">Projects</Link>
-                <Link to="/blog" className="nav-link">Blog</Link>
-              </ul>
-            </div>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="navbar-nav ms-auto">
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                `nav-link ${isActive ? 'active-link' : ''}`
+              }
+            >
+              about
+            </NavLink>
+            <NavLink
+              to="/projects"
+              className={({ isActive }) =>
+                `nav-link ${isActive ? 'active-link' : ''}`
+              }
+            >
+              projects
+            </NavLink>
+            <NavLink
+              to="/blog"
+              className={({ isActive }) =>
+                `nav-link ${isActive ? 'active-link' : ''}`
+              }
+            >
+              blog
+            </NavLink>
           </div>
         </div>
-      </div>
+      </nav>
     </>
   );
 }
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -6,6 +6,29 @@ import Journey from './journey.jsx'
 import Portfolio from './portfolio.jsx'
 import ProjectSlider from './projectslider.jsx'
 import { Link } from 'react-router-dom'
+import { TypeAnimation } from 'react-type-animation';
+ 
+const ExampleComponent = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        'One', // Types 'One'
+        2000, // Waits 1s
+        'Two', // Deletes 'One' and types 'Two'
+        2000, // Waits 2s
+        'Two Three',
+        2000, // Types 'Three' without deleting 'Two'
+        () => {
+          console.log('Sequence completed');
+        },
+      ]}
+      wrapper="span"
+      cursor={true}
+      repeat={Infinity}
+      style={{ fontSize: '1.3em', display: 'inline-block' }}
+    />
+  );
+};
 
 function Home() {
   return (
@@ -13,10 +36,13 @@ function Home() {
       {/* Bootstrap-style jumbotron */}
       <div className="jumbotron text-center different-section top-container container-lg  home-circle mb-5">
         <h1 className="display-6">Hello, world!</h1>
-        <p>
-          This is a simple hero unit, a jumbotron-style component for highlighting content.
-        </p>
+        <ExampleComponent />
         <hr className="my-4" />
+        <div class="d-flex justify-content-center mb-4 social-icons">
+          <ion-icon name="logo-github"></ion-icon>
+          <ion-icon name="logo-instagram"></ion-icon>
+          <ion-icon name="logo-linkedin"></ion-icon>
+        </div>
         <Link className="btn btn-primary btn-lg" to="/about" role="button">
           Learn more
         </Link>
