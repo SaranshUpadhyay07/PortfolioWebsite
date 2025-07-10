f38012affb56607e7d5ee9d3195169762c0b8a06
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2025-07-11 03:59:52 +0530

    Quote section added

--- src/components/quote.jsx
+++ src/components/quote.jsx
@@ -4,7 +4,7 @@ const Quote = () => {
   const [quote, setQuote] = useState(null);
 
   const fetchQuote = () => {
-    fetch('https://api.quotable.io/random')
+    fetch('https://api.quotable.io/random?tags=famous-quotes')
       .then((res) => res.json())
       .then((data) => {
         setQuote({
