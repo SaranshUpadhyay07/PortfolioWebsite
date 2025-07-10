4c47c8f27d46e64b7503782d6eaeef4455cc089c
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2025-07-10 19:24:23 +0530

    home page changed

--- src/components/projectslider.jsx
+++ src/components/projectslider.jsx
@@ -19,7 +19,7 @@ function ProjectsSlider() {
     <div className="different-section py-5 mb-5">
       <div className="container-lg px-lg-5 py-2 text-dark">
         <div className="px-lg-5">
-          <h2 className="fw-light mb-4 text-center mb-5">My Latest Projects</h2>
+          <h2 className="mb-5 fw-normal display-4 text-center text-md-start px-lg-3">My Latest Projects</h2>
           <div className="px-lg-5 projectslider-card">
             <div className="row custom-project-row ">
               {projectData.map((project, index) => (
